8b206adbb2cb25c3b2839fe33c96c300cbdd534e
Phillip Green <phillycheez11@gmail.com>
2018-01-22 22:13:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -68,4 +68,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array.pop();
   
 }
-
+function removeElementFromEndOfArray(array){
+ return array.slice(0, array.length - 1);
+  
+}
