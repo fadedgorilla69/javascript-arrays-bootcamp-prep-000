3dbe6dad34afcb3915d6a21d3f8484fb06441b91
Phillip Green <phillycheez11@gmail.com>
2018-01-22 22:08:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -52,7 +52,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 function accessElementInArray(array, index){
   
-    return array[3];
+    return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array, element){
