f109bd550fd299656b8bc9f03b361bc52ade9731
Phillip Green <phillycheez11@gmail.com>
2018-01-22 21:43:27 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+];
+ 
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+];
+
+var chocolateBars = [
+'snickers', 
+'hundred grand', 
+'kit kat', 
+'skittles'];
+
+var newArray = [1,];
+var newArray1 = [1,];
+var newArray2 = [1,];
+function addElementToBeginningOfArray(array, element){
+  ['foo',...newArray];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  newArray1.unshift('foo');
+  return newArray1;
+}
+
+function addElementToEndOfArray(array, element){
+  
+  newArray2.push("whatchyamacallit");
+  return newArray2;
+}
+
